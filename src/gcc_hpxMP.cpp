dc15c165d269905c2c2663d9f912478386cd5faa
tianyi93 <harter019@gmail.com>
2018-09-25 09:52:58 -0500

    organizing gcc_hpxMP.cpp

--- src/gcc_hpxMP.cpp
+++ src/gcc_hpxMP.cpp
@@ -6,85 +6,77 @@
 #include <iostream>
 #include <assert.h>
 
-using std::cout;
-using std::endl;
-
 extern boost::shared_ptr<hpx_runtime> hpx_backend;
 
 void
-__kmp_GOMP_fork_call(void(*unwrapped_task)(void *), microtask_t wrapper, int argc, ...) {
-    va_list ap;
-    va_start(ap, argc);
-    vector<void*> argv(argc);
-
-    for( int i = 0; i < argc; i++ ){
-        argv[i] = va_arg( ap, void * );
-    }
-    va_end( ap );
-    void ** args = argv.data();
-
-    hpx_backend->fork(__kmp_invoke_microtask, (microtask_t) wrapper, argc, args);
-
-}
-
-void
-__kmp_GOMP_microtask_wrapper(int *gtid, int *npr, void (*task)(void *),
-                             void *data) {
-    task(data);
-}
-
-void
-xexpand(KMP_API_NAME_GOMP_PARALLEL)(void (*task)(void *), void *data, unsigned num_threads, unsigned int flags) {
-    printf("GOMP_PARALLEL\n");
-#if HPXMP_HAVE_OMPT
-    ompt_pre_init();
-    ompt_post_init();
-#endif
-    start_backend();
-    //__kmpc_push_num_threads
-    omp_task_data * my_data = hpx_backend->get_task_data();
-    my_data->set_threads_requested(num_threads);
-
-    __kmp_GOMP_fork_call(task,(microtask_t )__kmp_GOMP_microtask_wrapper, 2, task, data);
-}
-
-void
-xexpand(KMP_API_NAME_GOMP_TASK)(void (*func)(void *), void *data, void (*copy_func)(void *, void *),
-                                long arg_size, long arg_align, bool if_cond, unsigned gomp_flags){
-//    printf("GOMP_TASK\n");
-    int gtid = hpx_backend->get_thread_num();
-
-    kmp_task_t *task = __kmpc_omp_task_alloc(nullptr, gtid, 0,
-                                        sizeof(kmp_task_t), arg_size ? arg_size + arg_align - 1 : 0,
-                                        (kmp_routine_entry_t)func);
-
-    if (arg_size > 0) {
-        if (arg_align > 0) {
-            task->shareds = (void *)((((size_t)task->shareds)
-                                      + arg_align - 1) / arg_align * arg_align);
-        }
-        if (copy_func) {
-            (*copy_func)(task->shareds, data);
-        }
-        else {
-            memcpy(task->shareds, data, arg_size);
-        }
-    }
-    if (if_cond) {
-        __kmpc_omp_task(nullptr, gtid, task);
-    }
-    else {
-        __kmpc_omp_task_begin_if0(nullptr, gtid, task);
-        func(data);
-        __kmpc_omp_task_complete_if0(nullptr, gtid, task);
-    }
-}
-
-void
-xexpand(KMP_API_NAME_GOMP_TASKWAIT)(void)
+xexpand(KMP_API_NAME_GOMP_BARRIER)(void)
 {
-//    printf("GOMP_TASKWAIT\n");
-    __kmpc_omp_taskwait(nullptr, 0);
+//    printf("GOMP_BARRIER\n");
+    __kmpc_barrier(nullptr, 0);
+}
+
+//
+// Mutual exclusion
+//
+
+//
+// The symbol that icc/ifort generates for unnamed for unnamed critical
+// sections - .gomp_critical_user_ - is defined using .comm in any objects
+// reference it.  We can't reference it directly here in C code, as the
+// symbol contains a ".".
+//
+// The RTL contains an assembly language definition of .gomp_critical_user_
+// with another symbol __kmp_unnamed_critical_addr initialized with it's
+// address.
+//
+
+void
+xexpand(KMP_API_NAME_GOMP_CRITICAL_START)(void)
+{
+//    printf("GOMP_CRITICAL_START\n");
+    __kmpc_critical(nullptr, 0, nullptr);
+}
+
+void
+xexpand(KMP_API_NAME_GOMP_CRITICAL_END)(void)
+{
+//    printf("GOMP_CRITICAL_END\n");
+    __kmpc_end_critical(nullptr, 0, nullptr);
+}
+
+void
+xexpand(KMP_API_NAME_GOMP_CRITICAL_NAME_START)(void **pptr)
+{
+//    printf("GOMP_CRITICAL_NAME_START\n");
+    __kmpc_critical(nullptr, 0, nullptr);
+}
+
+void
+xexpand(KMP_API_NAME_GOMP_CRITICAL_NAME_END)(void **pptr)
+{
+//    printf("GOMP_CRITICAL_NAME_END\n");
+    __kmpc_end_critical(nullptr, 0, nullptr);
+}
+
+//
+// The Gnu codegen tries to use locked operations to perform atomic updates
+// inline.  If it can't, then it calls GOMP_atomic_start() before performing
+// the update and GOMP_atomic_end() afterward, regardless of the data type.
+//
+// not being called by gcc here and in openmp
+void
+xexpand(KMP_API_NAME_GOMP_ATOMIC_START)(void)
+{
+    printf("KMP_API_NAME_GOMP_ATOMIC_START\n");
+    __kmp_acquire_atomic_lock(&__kmp_atomic_lock, 0);
+}
+
+//not being called by gcc here and in openmp
+void
+xexpand(KMP_API_NAME_GOMP_ATOMIC_END)(void)
+{
+    printf("KMP_API_NAME_GOMP_ATOMIC_END\n");
+    __kmp_release_atomic_lock(&__kmp_atomic_lock, 0);
 }
 
 int
@@ -139,57 +131,135 @@ void xexpand(KMP_API_NAME_GOMP_SINGLE_COPY_END)(void *data)
     hpx_backend->barrier_wait();
 }
 
+//
+// The parallel contruct
+//
+
+//Static if not in debug?
 void
-xexpand(KMP_API_NAME_GOMP_BARRIER)(void)
-{
-//    printf("GOMP_BARRIER\n");
-    __kmpc_barrier(nullptr, 0);
+__kmp_GOMP_microtask_wrapper(int *gtid, int *npr, void (*task)(void *),
+                             void *data) {
+    task(data);
 }
 
+//Static if not in debug?
 void
-xexpand(KMP_API_NAME_GOMP_CRITICAL_START)(void)
-{
-//    printf("GOMP_CRITICAL_START\n");
-    __kmpc_critical(nullptr, 0, nullptr);
+__kmp_GOMP_fork_call(void(*unwrapped_task)(void *), microtask_t wrapper, int argc, ...) {
+    va_list ap;
+    va_start(ap, argc);
+    vector<void*> argv(argc);
+
+    for( int i = 0; i < argc; i++ ){
+        argv[i] = va_arg( ap, void * );
+    }
+    va_end( ap );
+    void ** args = argv.data();
+
+    hpx_backend->fork(__kmp_invoke_microtask, (microtask_t) wrapper, argc, args);
+
+}
+
+//
+// Loop worksharing constructs
+//
+
+//
+// The Gnu codegen passes in an exclusive upper bound for the overall range,
+// but the libguide dispatch code expects an inclusive upper bound, hence the
+// "end - incr" 5th argument to KMP_DISPATCH_INIT (and the " ub - str" 11th
+// argument to __kmp_GOMP_fork_call).
+//
+// Conversely, KMP_DISPATCH_NEXT returns and inclusive upper bound in *p_ub,
+// but the Gnu codegen expects an excluside upper bound, so the adjustment
+// "*p_ub += stride" compenstates for the discrepancy.
+//
+// Correction: the gnu codegen always adjusts the upper bound by +-1, not the
+// stride value.  We adjust the dispatch parameters accordingly (by +-1), but
+// we still adjust p_ub by the actual stride value.
+//
+// The "runtime" versions do not take a chunk_sz parameter.
+//
+// The profile lib cannot support construct checking of unordered loops that
+// are predetermined by the compiler to be statically scheduled, as the gcc
+// codegen will not always emit calls to GOMP_loop_static_next() to get the
+// next iteration.  Instead, it emits inline code to call omp_get_thread_num()
+// num and calculate the iteration space using the result.  It doesn't do this
+// with ordered static loop, so they can be checked.
+//
+
+
+//
+// Tasking constructs
+//
+
+void
+xexpand(KMP_API_NAME_GOMP_TASK)(void (*func)(void *), void *data, void (*copy_func)(void *, void *),
+                                long arg_size, long arg_align, bool if_cond, unsigned gomp_flags){
+//    printf("GOMP_TASK\n");
+    int gtid = hpx_backend->get_thread_num();
+
+    kmp_task_t *task = __kmpc_omp_task_alloc(nullptr, gtid, 0,
+                                             sizeof(kmp_task_t), arg_size ? arg_size + arg_align - 1 : 0,
+                                             (kmp_routine_entry_t)func);
+
+    if (arg_size > 0) {
+        if (arg_align > 0) {
+            task->shareds = (void *)((((size_t)task->shareds)
+                                      + arg_align - 1) / arg_align * arg_align);
+        }
+        if (copy_func) {
+            (*copy_func)(task->shareds, data);
+        }
+        else {
+            memcpy(task->shareds, data, arg_size);
+        }
+    }
+    if (if_cond) {
+        __kmpc_omp_task(nullptr, gtid, task);
+    }
+    else {
+        __kmpc_omp_task_begin_if0(nullptr, gtid, task);
+        func(data);
+        __kmpc_omp_task_complete_if0(nullptr, gtid, task);
+    }
 }
 
 
 void
-xexpand(KMP_API_NAME_GOMP_CRITICAL_END)(void)
+xexpand(KMP_API_NAME_GOMP_TASKWAIT)(void)
 {
-//    printf("GOMP_CRITICAL_END\n");
-    __kmpc_end_critical(nullptr, 0, nullptr);
+//    printf("GOMP_TASKWAIT\n");
+    __kmpc_omp_taskwait(nullptr, 0);
 }
 
-void
-xexpand(KMP_API_NAME_GOMP_CRITICAL_NAME_START)(void **pptr)
-{
-//    printf("GOMP_CRITICAL_NAME_START\n");
-    __kmpc_critical(nullptr, 0, nullptr);
-}
+//
+// Sections worksharing constructs
+//
+
+//
+// For the sections construct, we initialize a dynamically scheduled loop
+// worksharing construct with lb 1 and stride 1, and use the iteration #'s
+// that its returns as sections ids.
+//
+// There are no special entry points for ordered sections, so we always use
+// the dynamically scheduled workshare, even if the sections aren't ordered.
+//
 
 
+// these are new GOMP_4.0 entry points
 void
-xexpand(KMP_API_NAME_GOMP_CRITICAL_NAME_END)(void **pptr)
-{
-//    printf("GOMP_CRITICAL_NAME_END\n");
-    __kmpc_end_critical(nullptr, 0, nullptr);
-}
+xexpand(KMP_API_NAME_GOMP_PARALLEL)(void (*task)(void *), void *data, unsigned num_threads, unsigned int flags) {
+    printf("GOMP_PARALLEL\n");
+#if HPXMP_HAVE_OMPT
+    ompt_pre_init();
+    ompt_post_init();
+#endif
+    start_backend();
+    //__kmpc_push_num_threads
+    omp_task_data * my_data = hpx_backend->get_task_data();
+    my_data->set_threads_requested(num_threads);
 
-// not being called by gcc here and in openmp
-void
-xexpand(KMP_API_NAME_GOMP_ATOMIC_START)(void)
-{
-    printf("KMP_API_NAME_GOMP_ATOMIC_START\n");
-    __kmp_acquire_atomic_lock(&__kmp_atomic_lock, 0);
-}
-
-//not being called by gcc here and in openmp
-void
-xexpand(KMP_API_NAME_GOMP_ATOMIC_END)(void)
-{
-    printf("KMP_API_NAME_GOMP_ATOMIC_END\n");
-    __kmp_release_atomic_lock(&__kmp_atomic_lock, 0);
+    __kmp_GOMP_fork_call(task,(microtask_t )__kmp_GOMP_microtask_wrapper, 2, task, data);
 }
 
 // GOMP_1.0 aliases
